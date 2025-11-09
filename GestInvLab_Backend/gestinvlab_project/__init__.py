20a5b0eb8ed6ff035e2c2d2b5e513cd899ae61a6
Alejano1 <a.colomafernandez@uandresbello.edu>
2025-11-09 20:07:30 -0300

    Delete GestInvLab_Backend directory

--- GestInvLab_Backend/gestinvlab_project/__init__.py
+++
@@ -1,4 +0,0 @@
-# Archivo: gestinvlab_project/__init__.py
-
-import pymysql
-pymysql.install_as_MySQLdb()
--- GestInvLab_Backend/inventory/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Servicio, Insumo, Lote, Movimiento, Detalle_Movimiento
-
-admin.site.register(Servicio)
-admin.site.register(Insumo)
-admin.site.register(Lote)
-admin.site.register(Movimiento)
-admin.site.register(Detalle_Movimiento)
